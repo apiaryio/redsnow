ef9fecf8601ea2b70881909817dd070f2dbe305e
Ladislav Prskavec <ladislav@apiary.io>
2014-10-29 21:51:34 +0100

    ClassAndModuleChildren fix

--- test/_helper.rb
+++ test/_helper.rb
@@ -6,7 +6,7 @@ require 'turn' unless ENV['TM_FILEPATH'] || ENV['CI']
 require 'mocha'
 require File.join(File.expand_path('../../lib/redsnow.rb', __FILE__))
 # Test Helper
-class Test::Unit::TestCase
+class TestHelper < Test::Unit::TestCase
   def fixture_file(name)
     File.read File.expand_path("../fixtures/#{name}", __FILE__)
   end
